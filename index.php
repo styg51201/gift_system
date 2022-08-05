cd06ae7dec3023903995f80cede932fc52282bb6
Stacey Zhan <styg51201@gmail.com>
2022-08-05 14:32:04 +0800

    新增禮品系統

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-//echo "<h1>stacey zhan</h1>";
-//echo $_SERVER["SERVER_NAME"];
 header('Location: ./gift/');
-
-// if($_SERVER["SERVER_NAME"] == 'localhost'){
-//   header('Location: ./gift/');
-// }else{
-//   header('Location: ./gift/');
-// }
 
 ?>
