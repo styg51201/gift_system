5891bc749d71b55c8b68b746d0d7601fb7b7da45
Stacey Zhan <styg51201@gmail.com>
2022-10-09 21:42:24 +0800

    creat gift_reset.php

--- gift/gift_reset.php
+++ gift/gift_reset.php
@@ -2,18 +2,23 @@
 include_once('../init/sl_init.php'); 
 
 
-
-if( $argv[0] == "stacey" ){
-
     $f_var['mdb'] = 'heroku'; // db name
     $f_var['con_db'] = sl_open($f_var['mdb']); // 開啟資料庫
+
+
+if( $argv[0] == "stacey" ){
 
     $sql = "UPDATE empno set `name` = concat(`name`,{date(Hi)})
                 where empno = 2030088";
 
-    $result = mysqli_query($f_var['con_db'],$sql);
 
+}else{
+  
+
+    $sql = "UPDATE empno set `name` = concat(`name`,{date(H)})
+                where empno = 2030088";
 }
 
+$result = mysqli_query($f_var['con_db'],$sql);
 
 ?>
