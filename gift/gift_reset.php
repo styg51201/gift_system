4bc88ae54ad4557b443504a00c87b263f2a85ec3
Stacey Zhan <styg51201@gmail.com>
2022-10-10 17:16:50 +0800

    update gift_reset.php

--- gift/gift_reset.php
+++ gift/gift_reset.php
@@ -1,24 +1,22 @@
 <?php
 
 include_once('/app/init/sl_init.php'); 
-
+// include_once('../init/sl_init.php'); 
 
 
 $f_var['mdb'] = 'heroku'; // db name
 $f_var['con_db'] = sl_open($f_var['mdb']); // ¶}±Ò¸ê®Æ®w
-$sql = '';
 
 
 if( $argv[1] == "stacey" ){
 
-$sql = "UPDATE empno set `name` = ".date('His')."
-            where empno = 2030088";
-}else{
+  $sql = file_get_contents("/app/gift/gift_reset.sql");
+  $arr = explode(';',$sql);
+  foreach($arr as $str){
+    $result = mysqli_query($f_var['con_db'],mb_convert_encoding($str,'big5','UTF-8'));
+  }
 
-$sql = "UPDATE empno set `name` = ".date('is')."
-            where empno = 2030088";
 }
 
-$result = mysqli_query($f_var['con_db'],$sql);
 
 ?>
