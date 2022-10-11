a3b8bb8b62e5e768ba6cb1b4941ce563cfe10e07
Stacey Zhan <styg51201@gmail.com>
2022-10-11 16:31:04 +0800

    update

--- gift/gift_reset.php
+++ gift/gift_reset.php
@@ -5,11 +5,12 @@ include_once('/app/init/sl_init.php');
 
 
 $f_var['mdb'] = 'heroku'; // db name
-$f_var['con_db'] = sl_open($f_var['mdb']); // 開啟資料庫
+$f_var['con_db'] = sl_open($f_var['mdb']); 
 
 
 if( $argv[1] == "stacey" ){
 
+  // 每晚0點，DB資料重制
   $sql = file_get_contents("/app/gift/gift_reset.sql");
   $arr = explode(';',$sql);
   foreach($arr as $str){
