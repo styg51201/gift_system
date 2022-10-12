0c3a45c02fb6eaea92724768f651830b85b80db2
Stacey Zhan <styg51201@gmail.com>
2022-10-12 23:01:33 +0800

    update

--- gift/gift_reset.php
+++ gift/gift_reset.php
@@ -8,16 +8,17 @@ $f_var['mdb'] = 'heroku'; // db name
 $f_var['con_db'] = sl_open($f_var['mdb']); 
 
 
-if( $argv[1] == "stacey" ){
+if( $argv[1] == "reset" ){
 
-  
-  // 每晚0點，DB資料重制
+  // 每晚0點重置DB資料
   $sql = file_get_contents("/app/gift/gift_reset.sql");
   $arr = explode(';',$sql);
   foreach($arr as $str){
     $result = mysqli_query($f_var['con_db'],mb_convert_encoding($str,'big5','UTF-8'));
   }
 
+}else{
+  header('Location: /app/gift/');
 }
 
 
--- readme.md
+++ readme.md
@@ -1,2 +1,17 @@
-gift
-text
+## 禮品系統
+
+實務上完成的禮品系統，網站架設於 Heroku 平台，有設定排程每日 00 點把資料重置，歡迎到網站上實際操作使用系統。
+
+**系統網址** : <a> https://sl-works-stacey.herokuapp.com</a>
+**原始碼** : <a> https://github.com/styg51201/gift_system </a>
+
+使用技術 :
+
+- 主要－PHP ( 使用樣板套件 TemplatePower )
+- 資料庫－MySQL
+- PHPExcel 套件（匯出 Excel 報表）
+- HTML + CSS + Jquery
+- Ajax 傳遞 ( json 格式 )
+- 版本控制 Git
+
+這個專案主管確認完需求後，是交由我一個人執行，包含了前端+後端以及資料表的規劃建置，大約 1 個半月的時間完成此專案。
